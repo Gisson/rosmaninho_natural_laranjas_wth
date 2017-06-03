56aa928193412392c73a33f743a1e782a87c6815
Gisson <jorge.heleno@tecnico.ulisboa.pt>
2017-06-03 11:20:08 +0000

    Add check for environment variable in run script

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+if [[ -z "$GITHUB_API_TOKEN" ]];then
+	echo "Token not set. please export GITHUB_API_TOKEN with your token"
+	echo "Continue?(press Ctrl+c to exit)"
+ 	read
+fi	
+
 python3 src/server.py
