0b2c6f00c6d3df8c670038ae9472b7921cdd7876
Gisson <jorge.heleno@tecnico.ulisboa.pt>
2017-06-03 11:17:14 +0000

    Revert "Add parallelization in partitioning"
    This reverts commit e552d8681f879f892217b9e18daf10b377b3af0f.
    
    Parallelization slew down the program instead of speeding it up

--- src/partitioner.py
+++ src/partitioner.py
@@ -18,7 +18,8 @@ class Partitioner:
         self.files = files
         self.author = author
 
-    def get_code_elements(self,fileblocks=[]):
+    def get_code_elements(self):
+        fileblocks=[]
         for fil in self.files:
             if not fil.patch: #its a blob 
                 continue
