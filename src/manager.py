3edfb3d554fc0a70e994ce5291c4530f04f26455
Rodrigo Rato <rodrigorato1996@hotmail.com>
2017-06-03 01:38:24 +0100

    Fixes previous commit

--- src/manager.py
+++ src/manager.py
@@ -9,6 +9,14 @@ class Manager:
     def __init__(self, user):
         self.user = user
     
+    def add_tech(self, techname):
+        # TODO
+        pass
+
+    def add_filter(self, filter_type, value):
+        # TODO
+        pass
+
     def rank(self):
         return { Keys.USER: self.user,
                 Keys.RANKINGS: { 'critA': 0,
