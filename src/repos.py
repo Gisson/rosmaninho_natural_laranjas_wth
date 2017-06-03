b537eabb6148ecb6280209e3e50150e51f905ad0
Rodrigo Rato <rodrigorato1996@hotmail.com>
2017-06-03 02:51:35 +0100

    Adds repos module with intended functionality

--- src/repos.py
+++ src/repos.py
@@ -1,17 +1,54 @@
 #!/usr/bin/env python3
 
 from github import Github
+import os
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.level = logging.INFO
 
 class Repos:
     def __init__(self, user):
+        logger.info("[REPOS] - Initializing")
         self.user = user
-        pass
+        self.techs = []
+        logger.info("[REPOS] - Instantiating the Github object")
+        self.github = Github(getApiToken())
+        logger.info("[REPOS] - Intialized!")
 
     def add_tech(self, tech):
-        # TODO
-        pass
+        logger.info("[REPOS] - Adding tech filter \"" + tech + "\"")
+        self.techs += [tech]
 
+    """
+        Returns a list of github.Repository.Repository objects
+        where all of the techs in self.techs can be found.
+    """
     def get_repos(self):
-        # TODO
-        return []
+        logger.info("[REPOS] - Starting the search for user " + self.user + "'s repos")
+        filtered_repos = []
+        logger.info("[REPOS] - Getting their repos")
+        repos = self.github.get_user(self.user).get_repos()
 
+        for repo in repos:
+            logger.info("[REPOS] - Checking their repos against the techs=" + str(self.techs))
+            if all(lang in repo.get_languages() for lang in self.techs):
+                filtered_repos += [repo]
+                logger.info("[REPOS] - " + repo.name + " matched!")
+            else:
+                logger.info("[REPOS] - " + repo.name + " did not match")
+
+        logger.info("[REPOS] - Returning the matching list of repos=" + str(filtered_repos))
+        return filtered_repos
+
+
+def getApiToken():
+    return os.environ['GITHUB_API_TOKEN']
+
+if(logger.level == logging.DEBUG):
+    # Just to test this one class
+    r = Repos("rodrigorato")
+    r.add_tech("Java")
+    for repo in r.get_repos():
+        print(repo.name)
